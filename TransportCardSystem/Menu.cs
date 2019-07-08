6cdf7bf826bbc16c7bbbfb38d0cc5db07a52d53f
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-07-08 13:57:10 +0600

    feat: добавлено меню с обработкой клавиш

---
+++ TransportCardSystem/Menu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCardSystem
+{
+    public class Menu
+    {
+       
+        private ConsoleColor _defaultTextColor { get; set; } = ConsoleColor.Gray;
+        private ConsoleColor _defaultBGColor { get; set; } = ConsoleColor.Black;
+        private ConsoleColor _selectTextColor { get; set; } = ConsoleColor.Yellow;
+        private ConsoleColor _selectBGColor { get; set; } = ConsoleColor.DarkBlue;
+        private int _selectPosition;
+
+        public Menu (){}
+
+        public int ShowMenu(List<string> menuItems, List<int> menuItemsId)
+        {
+            
+            
+            var input = Console.ReadKey();
+            
+            Console.Clear();
+            //do
+            //{
+            for (var i = 0; i < menuItems.Count; i++)
+                {
+                Console.SetCursorPosition(left: 0, top: 0 + i);
+                    if(_selectPosition == i)
+                    {
+                        Console.BackgroundColor = _selectBGColor;
+                        Console.ForegroundColor = _selectTextColor;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = _defaultBGColor;
+                        Console.ForegroundColor = _defaultTextColor;
+                    }
+                    Console.WriteLine(menuItems[i]);
+                }
+
+                Console.ResetColor();
+
+                switch(input.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        {
+                            if(_selectPosition > 0)
+                            {
+                                _selectPosition--;
+                            }
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            if(_selectPosition < menuItems.Count - 1)
+                            {
+                                _selectPosition++;                           
+                            }
+                            break;
+                        }
+                    case ConsoleKey.Enter:
+                        {
+                            return menuItemsId[_selectPosition];
+                        }
+                }
+            //} while (true);
+            return -1;
+        }
+    }
+}
--- TransportCardSystem/Program.cs
+++ TransportCardSystem/Program.cs
@@ -9,38 +9,69 @@ namespace TransportCardSystem
 {
     class Program
     {
-        
+        public enum MenuId
+        {
+            ID_BUY,
+            ID_RECHARGE,
+            ID_VALIDATE,
+            ID_EXIT,
+        }
 
-        
         static void Main (string[] args)
         {
 
-            var menuItems = new string[]
-            {
-                "Купить траспортную карту",
-                "Пополнить транспортную карту",
-                "Проверить баланс траспортной карты",
-                "Валидировать траспортную карту",
-                "Выйти"
-            };
-            for(var i = 0; i < menuItems.Length; i++)
-            {
-                if(i % 2 == 0)
-                {
-                    Console.BackgroundColor = ConsoleColor.DarkGray;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                }
-                else
-                {
-                    //Console.BackgroundColor = ConsoleColor.Black;
-                    Console.ResetColor();
-                }
-                Console.WriteLine($"{i + 1} - {menuItems[i]}");
-            }
-            Console.ResetColor();
-            Console.WriteLine("Выбрать:");
+            var menu = new Menu();
+            var isExit = false;
+            var menuItems = new List<string>();
+            var menuItemsId = new List<int>();
 
-            var userChoice = Console.ReadLine();
+            menuItems.Add("Купить траспортную карту");
+            menuItems.Add("Пополнить транспортную карту");
+            menuItems.Add("Валидировать траспортную карту");
+            menuItems.Add("Выйти");
+
+            menuItemsId.Add((int) MenuId.ID_BUY);
+            menuItemsId.Add((int) MenuId.ID_RECHARGE);
+            menuItemsId.Add((int) MenuId.ID_VALIDATE);
+            menuItemsId.Add((int) MenuId.ID_EXIT);
+            Console.CursorVisible = false;
+            //do
+            //{
+
+            while (!isExit)
+            {
+
+
+                var selectMenuItem = menu.ShowMenu(menuItems, menuItemsId);
+                switch (selectMenuItem)
+                {
+                    case (int) MenuId.ID_EXIT:
+                        {
+                            isExit = true;
+                            break;
+                        }
+                    case (int) MenuId.ID_BUY:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Купить карту");
+                            break;
+                        }
+                    case (int) MenuId.ID_RECHARGE:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Пополнить карту");
+                            break;
+                        }
+                    case (int) MenuId.ID_VALIDATE:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Валидировать карту");
+                            break;
+                        }
+
+                }
+            }
+            //} while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
