9aef1c655bdd3864f6421109986a0ab8d67d75dc
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-07-08 13:26:18 +0600

    feat: добавлено отображение меню

--- TransportCardSystem/Program.cs
+++ TransportCardSystem/Program.cs
@@ -4,12 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TransportCardSystem
 {
     class Program
     {
+        
+
+        
         static void Main (string[] args)
         {
+
+            var menuItems = new string[]
+            {
+                "Купить траспортную карту",
+                "Пополнить транспортную карту",
+                "Проверить баланс траспортной карты",
+                "Валидировать траспортную карту",
+                "Выйти"
+            };
+            for(var i = 0; i < menuItems.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                else
+                {
+                    //Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ResetColor();
+                }
+                Console.WriteLine($"{i + 1} - {menuItems[i]}");
+            }
+            Console.ResetColor();
+            Console.WriteLine("Выбрать:");
+
+            var userChoice = Console.ReadLine();
         }
     }
 }
--- TransportCardSystem/TransportCard.cs
+++ TransportCardSystem/TransportCard.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TransportCardSystem
 {
-    class TransportCard
+    public class TransportCard
     {
     }
 }
